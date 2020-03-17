f95bcdd1d7ae90e7bf181a7258bb56fb5f670f50
StephanieZou1 <stephanie.zou3@gmail.com>
2020-03-17 16:08:09 -0400

    update2

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,31 @@
+class Ride
 
+    @@all = []
+
+    attr_accessor :driver, :passenger, :distance
+
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def self.average_distance
+        #class method
+        #take total distance/total rides
+        #total distance is 
+        # @@all.distance / @all.length
+
+        total_distance = @@all.sum do |ride|
+            ride.distance
+        end
+        total_distance.to_f / @@all.length
+        binding.pry
+    end
+
+    def self.all
+        @@all
+    end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+stephanie = Driver.new("Stephanie")
+alisha = Driver.new("Jess")
+mary = Driver.new("Mary")
+
+ride1 = Ride.new("Stephanie", "Tiffany", "25")
+ride2 = Ride.new("Mary", "Nick", "100")
+ride3 = Ride.new("Stephanie", "Joslyn", "50")
+ride4 = Ride.new("Jess", "Brianna", "5000")
+
+maggie = Passenger.new("Maggie")
+cynthia = Passenger.new("Cynthia")
+
 
 # Put your variables here~!
 
 binding.pry
+
+"Keeping this here"
