90f25479710d67243d51f1a5f507161c1a538a10
github username <github email address>
2020-03-17 16:12:35 -0400

    covid19

--- tools/console.rb
+++ tools/console.rb
@@ -6,5 +6,12 @@ end
 
 
 # Put your variables here~!
-
+P1 = Passenger.new("Jonathan")
+P2 = Passenger.new("Mason")
+D1 = Driver.new("Ryad")
+D2 = Driver.new("Hamza")
+R1 = Ride.new(Ryad, Mason, 5.0)
+R2 = Ride.new(Hamza, Mason, 75.0)
+R3 = Ride.new(Ryad, Mason, 43.0)
+R4 = Ride.new(Ryad, Jonathan, 89.0)
 binding.pry
