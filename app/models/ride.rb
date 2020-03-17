007e10d119359d41df4e776dfd542fd6b1fd9f6a
replacem3nts <donny.landis@gmail.com>
2020-03-17 15:31:49 -0400

    updates files to meet all deliverables

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,24 @@
+class Ride
+attr_accessor :distance
+attr_reader :driver, :passenger
+
+    @@all = []
+    
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+        dist_array = all.map(&:distance)
+        dist_array.sum / dist_array.length
+    end
+
+end
 
