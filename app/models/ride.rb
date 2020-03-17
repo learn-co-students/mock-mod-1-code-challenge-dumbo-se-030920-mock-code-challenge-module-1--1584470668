1c131799eb4791541e256eb53dc230f2710aab4e
GaryZCLau <garyzclau@gmail.com>
2020-03-17 16:08:01 -0400

    mockchallenge cry

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,22 @@
+class Ride
 
+    attr_accessor :driver, :passenger, :distance
+    @@all = []
+    def initialize(driver, passenger, distance)
+        @driver = driver
+        @passenger = passenger
+        @distance = distance.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_distance
+       distance =  self.all.sum{|rides|rides.distance}
+       rideamount = self.all.length
+       average = distance / rideamount
+       average
+    end
+end
