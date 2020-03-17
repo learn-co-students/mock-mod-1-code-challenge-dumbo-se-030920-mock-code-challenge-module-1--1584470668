5bf9055b35269c091dc0ff68c57008cdd6fcc3f5
Asadhjafri <asadhjafri@gmail.com>
2020-03-17 16:08:04 -0400

    Asad

--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,8 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/passenger.rb'
+require_relative '../app/models/ride.rb'
+
+
 
 def reload
   load 'config/environment.rb'
@@ -6,5 +10,9 @@ end
 
 
 # Put your variables here~!
+custy1 = Passenger.new("Jafri", "Haneida, Tokyo Tower", "Alfred", "10 Miles")
+custy2 = Passenger.new("Kudo", "Kyoto", "Pennyworth", "15 Miles")
+
 
 binding.pry
+
