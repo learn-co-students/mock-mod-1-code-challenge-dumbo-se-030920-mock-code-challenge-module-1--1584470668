118e8a2b10f9f73c98a86f4f4687f42544d7b46d
isabelxklee <hello@isabelklee.co>
2020-03-17 15:45:34 -0400

    wrote methods for Driver class, Passenger class, and Ride class

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,46 @@
+class Driver
 
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def rides
+        Ride.all.select { |ride|
+            ride.driver == self
+        }
+    end
+
+    def total_distance
+        distances = self.rides.map { |ride|
+            ride.distance
+        }
+
+        distances.sum
+    end
+
+    def passenger_names
+        drivers_passengers = self.rides.map { |ride|
+            ride.passenger.name
+        }
+
+        drivers_passengers.uniq
+    end
+
+    def self.mileage_cap(distance)
+    # Take an argument of a distance (float)
+    # return an array of all Drivers who have driven over the mileage
+        Driver.all.select { |driver|
+            driver.total_distance >= distance
+        }
+        
+    end
+
+    def self.all
+        @@all
+    end 
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+isabel = Passenger.new("Isabel")
+kiki = Passenger.new("Kiki")
 
-# Put your variables here~!
+frankie = Driver.new("Frankie")
+diva = Driver.new("Diva")
+
+to_trader_joes = Ride.new(isabel, frankie, 3)
+to_the_gym = Ride.new(isabel, diva, 12.75)
+to_pizzeria = Ride.new(isabel, frankie, 99)
+to_the_pet_store = Ride.new(kiki, diva, 250)
+to_the_movies = Ride.new(kiki, frankie, 15)
+
+Ride.average_distance
+Passenger.premium_members
+Driver.mileage_cap(50)
 
 binding.pry
